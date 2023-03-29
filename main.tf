5e232ab59bf68d40100836d409f9c36929aa583e
Jeremy Jeffery <jeremyjeffery@gmail.com>
2023-03-29 16:35:47 -0400

    added igw

--- main.tf
+++ main.tf
@@ -21,6 +21,28 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-route-table"
+  }
+}
+
+
 resource "aws_subnet" "main" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.subnet_cidr_block
@@ -29,6 +51,11 @@ resource "aws_subnet" "main" {
   tags = {
     Name = "${var.project_name}-subnet"
   }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_security_group" "allow_web" {
