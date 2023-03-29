b7e690f57e3ca6e8e72dbeabbab09f0d51760582
Jeremy Jeffery <jeremyjeffery@gmail.com>
2023-03-29 16:29:49 -0400

    updated instance type

--- variables.tf
+++ variables.tf
@@ -33,7 +33,7 @@ variable "availability_zone" {
 
 variable "instance_type" {
   description = "The EC2 instance type for the Nginx server"
-  default     = "t2.micro"
+  default     = "t2.nano"
 }
 
 variable "ubuntu_ami_id" {
